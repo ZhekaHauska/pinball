28799262f342f031ee03203827338c76b5fe89ac
gsys <zhekauran@gmail.com>
2022-11-13 19:27:02 +0300

    fix rbg sensor

--- scripts/RGBCameraSensor3D.gd
+++ scripts/RGBCameraSensor3D.gd
@@ -1,12 +1,11 @@
 extends Spatial
 class_name RGBCameraSensor3D
-var camera_pixels = null
-
-onready var camera_texture := $Control/TextureRect/CameraTexture as Sprite
 
 func get_camera_pixel_encoding():
-	return camera_texture.get_texture().get_data().data["data"].hex_encode()
+	var im =  $Viewport.get_texture().get_data()
+	im.flip_y()
+	return im.data["data"].hex_encode()
 
 func get_camera_shape()-> Array:
-	return [$Viewport.size[0], $Viewport.size[1], 4]
+	return [$Viewport.size[1], $Viewport.size[0], 4]
 
