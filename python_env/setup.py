55754db199a1f29672c61671dd0e1714c3d6b2aa
gsys <zhekauran@gmail.com>
2022-12-09 12:33:56 +0300

    remove old copyright notice; add setup.py

---
+++ python_env/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pinball',
+    version='0.1',
+    description='Python interface for Pinball environment.',
+    url='https://github.com/ZhekaHauska/pinball',
+    author='Evgenij Dzhivelikyan',
+    author_email='zhekauran@gmal.com',
+    license='The MIT License',
+    py_modules=['pinball']
+)
